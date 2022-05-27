fbd34f7008ba9069908ce0e6cb73df8d8fea5199
Ol-create <oluola96@gmail.com>
2022-05-27 04:11:52 -0600

    feat: create detaile component

--- src/components/CountryData.js
+++ src/components/CountryData.js
@@ -1,13 +1,30 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+import { BsArrowRightCircle } from 'react-icons/bs';
 
 const CountryData = (props) => {
   const { country } = props;
 
+  const imgURL = `https://countryflagsapi.com/png/${country.name}`;
+
   return (
-    <li>
-      <h2>{country.name}</h2>
-      <p>{country.totalCases}</p>
-    </li>
+    <Link
+      to={`/country/${country.name}`}
+      className="country-box"
+    >
+      <img src={imgURL} alt="" />
+
+      <BsArrowRightCircle />
+      <div>
+        <h2>{country.name}</h2>
+        <p>
+          {country.totalCases}
+          {' '}
+          <span>cases</span>
+        </p>
+      </div>
+    </Link>
   );
 };
 
